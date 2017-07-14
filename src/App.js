76f5e127476e76bfde2bb5680857878a36cb5445
Chris Clarke <chrisclarke@Admins-MacBook-Pro.local>
2017-07-14 14:59:06 +0100

    first attempt at a react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+export default ReactDOM;
