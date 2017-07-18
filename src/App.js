15cb410ed8ffb560d9551fd4ac483cd151861658
Chris Clarke <chrisclarke@Admins-MacBook-Pro.local>
2017-07-18 12:09:25 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import {list} from'./App.css';
+import _ from 'lodash';
 
 class App extends Component {
 
   render() {
     // const blocks = ['', '', '', '', '','', '', '', '', ''];
     // const listItems = blocks.map((blocks) => <div className='list-'>{blocks}</div>);
-    var data = ['Michael Jackson', 'Bob Dylan', 'Kendrick Lama'];
-    var listItems = data.map((value, index) => (
-      <li
-        key={value}
-        className={this.props.active === index ? 'active' : ''}>
-        {value}
-      </li>
-    ));
+      // var rows = [];
+      // for (var i=0; i < numrows; i++) {
+      //     rows.push(<ObjectRow />);
+      // }
+    // var data = ['list-1'];
+    // var listItems = data.map((value, index) => (
+    //   <div
+    //     key={value}
+    //     className={this.props.active === index ? 'active' : ''}>
+    //     {value}
+    //   </div>
+    // ));
     return (
       <div className='demo-container'>
-        <div>{listItems}</div>
+          {_.times(21, i =>
+              <div key={i} className={'list1'}>{i}.</div>
+          )}
       </div>
     );
   }
 }
 
 export default App;
+
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="container">
+//                 <ol>
+//                     {_.times(3, i =>
+//                         <li key={i}>
+//                             <Select onSelect={this.onSelect}>
+//                                 <option value="1">bacon</option>
+//                                 <option value="2">cheez</option>
+//                             </Select>
+//                         </li>
+//                     )}
+//                 </ol>
+//             </div>
+//         );
+//     }
+// }
+
+// <div>{listItems}</div>
